cf68e7325eb4baba139e3f7017606f1fd2482c31
magmac12 <74375369+magmac12@users.noreply.github.com>
2021-02-06 10:32:55 -0600

    Update Hero.js

--- src/components/Body/Hero/Hero.js
+++ src/components/Body/Hero/Hero.js
@@ -119,13 +119,13 @@ const Hero = () => {
             <Container>
                 <InnerWrapper>
                     <TextSection>
-                        <H1 inverse >A DeFi Analytics Community</H1>
-                        <Paragraph inverse marginBottom >Track the whales (DeFi market movers) with realtime data. See large volume transactions and traders on Uniswap, SushiSwap, and 1inch. First class community (DAO) - Learn more 👇 👇</Paragraph>
+                        <H1 inverse >Lower your DeFi brain fatigue</H1>
+                        <Paragraph inverse marginBottom >We curate DEX data for our token holding members. Whale tracking on Uniswap, SushiSwap, and 1inch. First class  (DAO). Join our free channels for a preview👇 👇</Paragraph>
                         <Button text="Join UniWhales (Free)" href="https://t.me/uniwhales" target="_blank"/>
-                        <TelegramText>5800+ members</TelegramText>
+                        <TelegramText>7000+ members</TelegramText>
                         <H1 inverse></H1>
                         <Button text="Join SushiWhales (Free)" href="https://t.me/sushiwhale" target="_blank"/>
-                        <TelegramText>450+ members</TelegramText>
+                        <TelegramText>650+ members</TelegramText>
                     </TextSection>
                     <ImageSection>
                         <img src={Dashboard} alt="Uniwhales Dashboard" />
