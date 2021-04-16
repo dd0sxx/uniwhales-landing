ca6b006a01a1958f37f60fb721145704ee7ecc4f
John <jkuney17@gmail.com>
2021-04-16 10:41:55 -0500

    add mailerlite form

--- src/components/Body/Newsletter/Newsletter.js
+++ src/components/Body/Newsletter/Newsletter.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styled from 'styled-components'
 import * as palette from '../../../palette.js';
 import Container from '../../Container/Container.js';
@@ -16,14 +16,33 @@ const Wrapper = styled.div`
   }
 `;
 
-
 const Hero = () => {
+
+    useEffect(() => {
+        const js = `
+        (function(m,a,i,l,e,r){ m['MailerLiteObject']=e;function f(){
+            var c={ a:arguments,q:[]};var r=this.push(c);return "number"!=typeof r?r:f.bind(c.q);}
+            f.q=f.q||[];m[e]=m[e]||f.bind(f.q);m[e].q=m[e].q||f.q;r=a.createElement(i);
+            var _=a.getElementsByTagName(i)[0];r.async=1;r.src=l+'?v'+(~~(new Date().getTime()/1000000));
+            _.parentNode.insertBefore(r,_);})(window, document, 'script', 'https://static.mailerlite.com/js/universal.js', 'ml');
+            
+            var ml_account = ml('accounts', '2651162', 'b4k0z4a1e7', 'load');
+        `;
+        const script = document.createElement("script");
+        const scriptText = document.createTextNode(js);
+        script.appendChild(scriptText);
+        document.body.appendChild(script);
+      }, [])
+
     return (
         <Wrapper>
             <Container>
                 <H2 nomarginBottom>Stay in the loop</H2>
-                <Paragraph>Join our email list for latest developments in the UniWhales ecosystem</Paragraph>
-                <Button inverse text="Join" href="https://docs.google.com/forms/d/e/1FAIpQLSfm2lwbKO4C58CG4KReY-GOLLuDM4S8oobXr1VLAGTNkYhB5g/viewform?usp=sf_link" />
+                <Paragraph style={{marginBottom: '10px'}}>Join our email list for latest developments in the UniWhales ecosystem</Paragraph>
+                <div className="ml-form-embed"
+                    data-account="2651162:b4k0z4a1e7"
+                    data-form="3935734:o1f0g0">
+                </div>
             </Container>
         </Wrapper>
 	)
